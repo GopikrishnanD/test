cfb75abd1a539d4cea5cc24f7487f68e52b9770d
GopikrishnanD <mailtodsgopi@gmail.com>
2020-02-27 20:28:13 +0530

    push war

---
+++ tmp/Controller/Enter Controlldsfgfdsger Name.cs
@@ -0,0 +1,691 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Molina.EnterpriseService.Cache;
+using System.ServiceModel.Web;
+using Molina.EnterpriseService.BusinessLayer;
+namespace sdgfg
+{
+	public class Enter Controlldsfgfdsger Name: ApiController
+	{
+
+	  dsfg dsfg = new dsfg();
+
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberAuthorization")] 
+		public MemberAuthorizationResponse GetMemberAuthorization(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberAuthorization(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberEnrollment")] 
+		public MemberEnrollmentResponse GetMemberEnrollment(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberEnrollment(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberPcp")] 
+		public MemberPcpResponse GetMemberPcp(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberPcp(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberMemo")] 
+		public MemberMemoResponse GetMemberMemo(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberMemo(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberClaims")] 
+		public MemberClaimResponse GetMemberClaims(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberClaims(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpPut]  
+		[Route("api/Enter Controlldsfgfdsger Name/UpdateQNXTMailingAddress")] 
+		public MemberQNXTMailingAddressUpdateResponse UpdateQNXTMailingAddress(MemberQNXTMailingAddressUpdateRequest memberQNXTMailingAddrUpdateRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.UpdateQNXTMailingAddress(memberQNXTMailingAddrUpdateRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetEnrollRestriction")] 
+		public EnrollRestrictionResponse GetEnrollRestriction(EnrollRestrictionRequest enrollRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetEnrollRestriction(enrollRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetRateCode")] 
+		public RateCodeResponse GetRateCode(RateCodeRequest memberRateRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetRateCode(memberRateRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetAttribute")] 
+		public AttributeResponse GetAttribute(AttributeRequest memberAttributeRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetAttribute(memberAttributeRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetPlanAction")] 
+		public MemberPlanActionResponse GetPlanAction(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetPlanAction(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpDelete]  
+		[Route("api/Enter Controlldsfgfdsger Name/DeletePlanAction")] 
+		public MemberPlanActionResponse DeletePlanAction(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.DeletePlanAction(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpPost]  
+		[Route("api/Enter Controlldsfgfdsger Name/CreatePlanAction")] 
+		public MemberPlanActionResponse CreatePlanAction(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.CreatePlanAction(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberMemoAlertIndicator")] 
+		public MemoAlertIndicatorResponse GetMemberMemoAlertIndicator(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberMemoAlertIndicator(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberMemoHedisIndicator")] 
+		public MemoAlertIndicatorResponse GetMemberMemoHedisIndicator(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberMemoHedisIndicator(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberPrimaryPCP")] 
+		public PrimaryPCPReponse GetMemberPrimaryPCP(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberPrimaryPCP(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberProfileSummary")] 
+		public MemberProfileSummaryResponse GetMemberProfileSummary(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberProfileSummary(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberAgencyName")] 
+		public MemberAgencyNameResponse GetMemberAgencyName(MemberAgencyNameRequest AgencyNameRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberAgencyName(AgencyNameRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberShareOfCost")] 
+		public ShareOfCostReponse GetMemberShareOfCost(ShareOfCostRequest costRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberShareOfCost(costRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberBasicUMDetails")] 
+		public UMBasicResponse GetMemberBasicUMDetails(UMDocRequest UMDocRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberBasicUMDetails(UMDocRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetUMDiagsAndServices")] 
+		public UMDiagsAndServicesResponse GetUMDiagsAndServices(UMDocRequest UMDocRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetUMDiagsAndServices(UMDocRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetUMAttributes")] 
+		public AttributeDetailsResponse GetUMAttributes(UMDocRequest memberAttributeRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetUMAttributes(memberAttributeRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetHedisAlert")] 
+		public HedisAlertResponse GetHedisAlert(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetHedisAlert(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetIdCardCompletionDate")] 
+		public IdCardCompletionResponse GetIdCardCompletionDate(MemberRequest memberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetIdCardCompletionDate(memberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetHistoricalAddress")] 
+		public HistoricalAddressResponse GetHistoricalAddress(HistoricalAddressRequest HistoricalAddressRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetHistoricalAddress(HistoricalAddressRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMarketplaceDetail")] 
+		public MarketPlaceResponse GetMarketplaceDetail(MarketPlaceRequest MarketPlaceRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMarketplaceDetail(MarketPlaceRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetAccountStatus")] 
+		public AccountStatusResponse GetAccountStatus(AccountStatusRequest AccountStatusRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetAccountStatus(AccountStatusRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetBenefitPlan")] 
+		public BenefitPlanResponse GetBenefitPlan(BenefitPlanRequest benefitPlanRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetBenefitPlan(benefitPlanRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetBenefitPlanAccumulation")] 
+		public BenefitPlanAccumulationResponse GetBenefitPlanAccumulation(BenefitPlanAccumulationRequest benefitPlanAccumulationRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetBenefitPlanAccumulation(benefitPlanAccumulationRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetAssociatedMembers")] 
+		public AssociatedMembersResponse GetAssociatedMembers(AssociatedMembersRequest associatedMemberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetAssociatedMembers(associatedMemberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetProfileSummary")] 
+		public ProfileSummaryResponse GetProfileSummary(ProfileSummaryRequest profileSummaryRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetProfileSummary(profileSummaryRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetPrimaryEnrollment")] 
+		public PrimaryEnrollmentResponse GetPrimaryEnrollment(PrimaryEnrollmentRequest primaryEnrollmentRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetPrimaryEnrollment(primaryEnrollmentRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetSecondaryEnrollment")] 
+		public SecondaryEnrollmentResponse GetSecondaryEnrollment(SecondaryEnrollmentRequest SecondaryEnrollmentRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetSecondaryEnrollment(SecondaryEnrollmentRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetCOBDetails")] 
+		public COBDetailsResponse GetCOBDetails(COBDetailsRequest cobDetailsRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetCOBDetails(cobDetailsRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetEnrollPlanActions")] 
+		public EnrollPlanActionsResponse GetEnrollPlanActions(EnrollPlanActionsRequest enrollPlanActionsRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetEnrollPlanActions(enrollPlanActionsRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/SearchMember")] 
+		public MemberSearchResponse SearchMember(SearchMemberRequest searchMemberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.SearchMember(searchMemberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/SearchPreEnrolledMember")] 
+		public SearchPreEnrolledMemberResponse SearchPreEnrolledMember(SearchPreEnrolledMemberRequest searchPreEnrolledMemberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.SearchPreEnrolledMember(searchPreEnrolledMemberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetPreEnrolledMemberDetail")] 
+		public PreEnrolledMemberDetailResponse GetPreEnrolledMemberDetail(PreEnrolledMemberDetailRequest searchPreEnrolledMemberRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetPreEnrolledMemberDetail(searchPreEnrolledMemberRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetResendStatus")] 
+		public ResendStatusResponse GetResendStatus(ResendStatusRequest resendStatusRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetResendStatus(resendStatusRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetMemberInvoice")] 
+		public MemberInvoiceResponse GetMemberInvoice(MarketPlaceRequest request) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetMemberInvoice(request); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetClaimExplanationOfBenefits")] 
+		public EOBPDFDetailResponse GetClaimExplanationOfBenefits(EOBPDFDetailRequest request) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetClaimExplanationOfBenefits(request); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetResponsiblePartyAddress")] 
+		public ResponsiblePartyAddressResponse GetResponsiblePartyAddress(ResponsiblePartyAddressRequest responsiblePartyAddressRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetResponsiblePartyAddress(responsiblePartyAddressRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+		[HttpGet]  
+		[Route("api/Enter Controlldsfgfdsger Name/GetClaimDetails")] 
+		public QNXTClaimSvc.GetClaimDetailResp GetClaimDetails(ClaimsServiceRequest claimServiceRequest) 
+		{ 
+ 
+			try 
+			{  
+				return dsfg.GetClaimDetails(claimServiceRequest); 
+			} catch (Exception ex)
+			 {
+				 throw ex;
+			 }
+ 
+			return  null;
+		}
+ 
+	}
+
+}
